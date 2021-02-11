8613ce2d89a0cf5d1e64489da2fa962e7261cf7a
ireneefr <irene.fernandez@alum.esci.upf.edu>
2021-02-11 17:47:21 +0100

    New app

--- myapp/server.R
+++ myapp/server.R
@@ -5,7 +5,6 @@ shinyServer(function(input, output, session) {
     
     # Max size
     options(shiny.maxRequestSize = 8000 * 1024^2) # 5MB getShinyOption("shiny.maxRequestSize") | 30*1024^2 = 30MB
-
     
     # Reaction of home action buttons
     observeEvent(input$b_qc, {
@@ -67,7 +66,311 @@ shinyServer(function(input, output, session) {
         }
     })
     
-    # Enable load button every time file input is updated
+    # input_data: enable b_input_data
     observeEvent(input$input_data, shinyjs::enable("b_input_data"))
+    
+    ################################
+    ########## rval_sheet() METHARRAY SHEET ##########
+    
+    # When you press button_input_load, the data is unzipped and the metharray sheet is loaded
+    rval_sheet <- eventReactive(input$b_input_data, {
+        print("OK")
+        # Check if updated file is .zip
+        validate(need(tools::file_ext(input$input_data$datapath) == "zip", "File extension should be .zip"))
+        
+        shinyjs::disable("button_input_load") # disable the load button to avoid multiple clicks
+        
+        if (dir.exists(paste0(tempdir(), "/experiment_data"))) {
+            unlink(paste0(tempdir(), "/experiment_data"), recursive = TRUE) # remove current files in target directory
+        }
+        
+        zip::unzip(input$input_data$datapath,
+                   exdir = paste0(tempdir(), "/experiment_data")
+        ) # extracting zip
+        
+        sheet <- minfi::read.metharray.sheet(paste0(tempdir(), "/experiment_data"))
+        
+        # We check if sheet is correct
+        # This is to prevent app crashes when zip updated is not correct.
+        validate(
+            need(
+                is.data.frame(sheet) &
+                    any(colnames(sheet) %in% "Slide") &
+                    any(colnames(sheet) %in% "Array"),
+                "SampleSheet is not correct. Please, check your samplesheet and your zip file."
+            )
+        )
+        
+        validate(
+            need(
+                anyDuplicated(colnames(sheet)) == 0,
+                "Repeated variable names are not allowed. Please, modify your sample sheet."
+            )
+        )
+        
+        colnames(sheet) <- make.names(colnames(sheet)) # fix possible not-valid colnames
+        
+        sheet
+    })
+    
+    
+    rval_sheet_target <- eventReactive(
+        input$button_input_next,
+        rval_sheet()[rval_sheet()[[input$select_input_samplenamevar]] %in% input$selected_samples, ]
+    )
+    
+    
+    rval_clean_sheet_target <- eventReactive(rval_gset(), {
+        generate_clean_samplesheet(target_samplesheet = minfi::pData(rval_gset()),
+                                   donorvar = input$select_input_donorvar)
+        
+    })
+    
+    
+    
+    
+    # When you press button_input_load, the form options are updated
+    observeEvent(input$b_input_data, {
+        updateSelectInput(
+            session,
+            "select_input_samplenamevar",
+            label = "Select Sample Names Column:",
+            choices = colnames(rval_sheet())
+        )
+        
+        updateSelectInput(
+            session,
+            "select_input_groupingvar",
+            label = "Select Variable of Interest:",
+            choices = colnames(rval_sheet())
+        )
+        updateSelectInput(
+            session,
+            "select_input_donorvar",
+            label = "Select Donor Variable:",
+            choices = colnames(rval_sheet())
+        )
+        updateSelectInput(
+            session,
+            "select_input_sex",
+            label = "Select Sex Column",
+            choices = c("None", colnames(rval_sheet()))
+        )
+        
+        
+        shinyjs::enable("button_input_next") # Enable button continue
+    })
+    
+    
+    # The checkbox of samples to process is updated when samplenamevar changes
+    observeEvent({input$select_input_samplenamevar
+        input$select_input_groupingvar},
+        updatePickerInput(
+            session,
+            "selected_samples",
+            label = "Select Samples to Process:",
+            selected = rval_sheet()[, input$select_input_samplenamevar],
+            choices = rval_sheet()[, input$select_input_samplenamevar],
+            choicesOpt = list(subtext = paste("Group: ", rval_sheet()[, input$select_input_groupingvar]))
+        )
+    )
+    
+    # when samples selected are changed, continue button is enabled again
+    observeEvent(input$selected_samples, shinyjs::enable("button_input_next"))
+    
+    # The dataframe is rendered
+    output$samples_table <- DT::renderDT(
+        rval_sheet(),
+        rownames = FALSE,
+        selection = "single",
+        style = "bootstrap",
+        options = list(
+            pageLength = 10,
+            autoWidth = TRUE,
+            scrollX = TRUE,
+            columnDefs = list(list(
+                targets = match("Basename", colnames(rval_sheet())) - 1, visible = FALSE
+            ))
+        )
+    )
+    
+    
+    
+    ########## rval_rgset() RGCHANNEL ##########
+    
+    # rval_rgset loads RGSet using read.metharray.exp and the sample sheet (rval_sheet())
+    rval_rgset <- eventReactive(input$button_input_next, ignoreNULL = FALSE, {
+        validate(need(input$fileinput_input != "", "Data has not been uploaded yet"))
+        
+        # Prior check to test variable selection
+        if (anyDuplicated(rval_sheet_target()[, input$select_input_samplenamevar]) > 0 |
+            anyDuplicated(rval_sheet_target()[, input$select_input_groupingvar]) == 0) {
+            showModal(
+                modalDialog(
+                    title = "Variable error",
+                    "Check if selected variables are correct. Sample Name Variable should not have duplicated values
+          and the variable of interest should have groups greater than 1.",
+                    easyClose = TRUE,
+                    footer = NULL
+                )
+            )
+        }
+        
+        # Check prior conditions to read data
+        validate(need(
+            anyDuplicated(rval_sheet_target()[, input$select_input_samplenamevar]) == 0,
+            "Sample Name Variable should not have duplicated values"
+        ))
+        validate(need(
+            anyDuplicated(rval_sheet_target()[, input$select_input_groupingvar]) > 0,
+            "Grouping variable should have groups greater than 1"
+        ))
+        
+        # disable button to avoid multiple clicks
+        shinyjs::disable("button_input_next")
+        
+        
+        # We need to check if this step works
+        withProgress(
+            message = "Reading array data...",
+            value = 2,
+            max = 5,
+            {
+                try({
+                    RGSet <- read_idats(
+                        targets = rval_sheet_target())
+                })
+                
+                if (!exists("RGSet", inherits = FALSE)) {
+                    showModal(
+                        modalDialog(
+                            title = "reading error",
+                            "Minfi can't read arrays specified in your samplesheet. Please, check your zipfile and your sampleSheet",
+                            easyClose = TRUE,
+                            footer = NULL
+                        )
+                    )
+                    shinyjs::disable("button_minfi_select")
+                }
+                
+                validate(
+                    need(
+                        exists("RGSet", inherits = FALSE),
+                        "Minfi can't read arrays specified in your samplesheet. Please, check your zipfile and your sampleSheet"
+                    )
+                )
+                
+                #Checking array type and annotation
+                nProbes = length(minfi::featureNames(RGSet))
+                
+                if(nProbes >= 622000 & nProbes <= 623000){
+                    
+                    if (!requireNamespace("IlluminaHumanMethylation450kanno.ilmn12.hg19", quietly = TRUE) |
+                        !requireNamespace("IlluminaHumanMethylation450kmanifest", quietly = TRUE))
+                    {
+                        showModal(
+                            modalDialog(
+                                title = "Missing package(s)",
+                                "450k annotation or manifest packages are not available. Please, install IlluminaHumanMethylation450kmanifest and IlluminaHumanMethylation450kanno.ilmn12.hg19 packages and restart the application.",
+                                easyClose = TRUE,
+                                footer = NULL
+                            )
+                        )
+                    }
+                    
+                    validate(
+                        need(
+                            requireNamespace("IlluminaHumanMethylation450kanno.ilmn12.hg19", quietly = TRUE) &
+                                requireNamespace("IlluminaHumanMethylation450kmanifest", quietly = TRUE),
+                            "450k annotation or manifest packages are not available. Please, install IlluminaHumanMethylation450kmanifest and IlluminaHumanMethylation450kanno.ilmn12.hg19 packages."
+                        )
+                    )
+                }
+                else if (nProbes >= 1032000 & nProbes <= 1053000){
+                    
+                    if (!requireNamespace("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", quietly = TRUE) |
+                        !requireNamespace("IlluminaHumanMethylationEPICmanifest", quietly = TRUE))
+                    {
+                        showModal(
+                            modalDialog(
+                                title = "Missing package(s)",
+                                "EPIC annotation or manifest packages are not available. Please, install IlluminaHumanMethylationEPICmanifest and IlluminaHumanMethylationEPICanno.ilm10b4.hg19 packages and restart the application.",
+                                easyClose = TRUE,
+                                footer = NULL
+                            )
+                        )
+                    }
+                    
+                    validate(
+                        need(
+                            requireNamespace("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", quietly = TRUE) &
+                                requireNamespace("IlluminaHumanMethylationEPICmanifest", quietly = TRUE),
+                            "EPIC annotation or manifest packages are not available. Please, install IlluminaHumanMethylationEPICmanifest and IlluminaHumanMethylationEPICanno.ilm10b4.hg19 packages."
+                        )
+                    )
+                }
+                
+                # analysis restarted
+                rval_analysis_finished(FALSE)
+                rval_dmrs_finished(FALSE)
+                
+                # we return RGSet
+                RGSet
+            }
+        )
+    })
+    
+    # We change the page to the next one
+    observeEvent(input$button_input_next, {
+        # check if rgset is loaded
+        req(rval_rgset())
+        
+        updateSelectInput(
+            session,
+            "select_slide",
+            choices = rval_sheet_target()$Slide
+        )
+        
+        updateSelectInput(
+            session,
+            "select_control",
+            selected = "BISULFITE CONVERSION I"
+        )
+        
+        # update PCA parameters
+        updateSelectInput(
+            session,
+            "select_minfi_pcaplot_pcx",
+            choices = paste0("PC", seq_len(nrow(rval_sheet_target()))),
+            selected = "PC1"
+        )
+        
+        updateSelectInput(
+            session,
+            "select_minfi_pcaplot_pcy",
+            choices = paste0("PC", seq_len(nrow(rval_sheet_target()))),
+            selected = "PC2"
+        )
+        
+        updateSelectInput(
+            session,
+            "select_minfi_pcaplot_color",
+            choices = c(
+                colnames(rval_sheet_target()),
+                "xMed",
+                "yMed",
+                "predictedSex"
+            ),
+            
+            selected = input$select_input_groupingvar
+        )
+        
+        shinyjs::enable("button_minfi_select")
+        #updateTabItems(session, "menu", "normalization")
+    })
+    
+    
+    
+    
     
 })
--- myapp/ui.R
+++ myapp/ui.R
@@ -1,5 +1,38 @@
+# AVAILABLE METHODS
+norm_options <- c(
+    "Raw",
+    "Illumina",
+    "Funnorm",
+    "Noob",
+    "SWAN",
+    "Quantile",
+    "Noob+Quantile"
+)
+
+hclust_methods <- c(
+    "single",
+    "complete",
+    "average",
+    "mcquitty",
+    "median",
+    "centroid"
+)
+
+controlNames <- c(
+    "BISULFITE CONVERSION I",
+    "BISULFITE CONVERSION II",
+    "HYBRIDIZATION",
+    "SPECIFICITY I",
+    "SPECIFICITY II",
+    "TARGET REMOVAL",
+    "BISULFITE CONVERSION I",
+    "EXTENSION",
+    "STAINING",
+    "NON-POLYMORPHIC"
+)
 
 library(shiny)
+library(shinydashboard)
 library(shinydashboardPlus)
 
 shinyUI(
@@ -28,29 +61,228 @@ shinyUI(
             
             tabItem(
                 tabName = "home",
-                verticalLayout(
+                verticalLayout( 
                     boxPlus(title = "INPUT DATA", width = 12, closable = FALSE, collapsible = TRUE, 
                             fileInput("input_data", "Upload input data (.zip)", multiple = FALSE, accept = ".zip"),
                             uiOutput("ui_input_data")
                     ),
+                    fluidRow(style = "margin-left: 2px; margin-right: 2px",
+                    conditionalPanel(
+                        "typeof output.samples_table != 'undefined'",
+                        boxPlus(title = "SELECTION OPTIONS", id = "selection_options",
+                                closable = FALSE, collapsible = TRUE,
+                            width = 3,
+                            selectInput("select_input_samplenamevar", "", c()),
+                            selectInput("select_input_groupingvar", "", c()),
+                            selectInput("select_input_donorvar", "", c()),
+                            pickerInput(
+                                inputId = "selected_samples",
+                                label = "",
+                                choices = c(),
+                                options = list(
+                                    `actions-box` = TRUE,
+                                    size = 10,
+                                    `selected-text-format` = "count > 3"
+                                ),
+                                multiple = TRUE
+                            ),
+                            selectInput("select_input_sex", "", c()),
+                            actionButton("button_input_next", "Continue to Analysis")
+                        )
+                    ),
+                    
+                    # Box 2: Table
+                    conditionalPanel(
+                        "input.b_input_data > 0", boxPlus(title = "SAMPLES TABLE",
+                                                          closable = FALSE, collapsible = TRUE,
+                        width = 9,
+                        withSpinner(DT::DTOutput("samples_table"))
+                        
+                    ))),
+                    conditionalPanel("input.button_input_next > 0",
                     splitLayout(style = "margin-left: 14px; margin-right: 14px;", cellArgs = list(style = "padding: 4px;"),
-                        actionButton("b_qc", width = "100%", label = strong("QC"), class = "btn-primary", style = "color:#fff; padding:80px; font-size:300%"),
-                        actionButton("b_exploratory_analysis", width = "100%",label = strong(HTML("Exploratory <br/> Analysis")), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%"),
-                        actionButton("b_dmp_dmr", width = "100%", label = strong("DMPs/DMRs"), class = "btn-primary", style = "color:#fff; padding:80px; font-size:300%"),
-                        actionButton("b_functional_enrichment", width = "90%",label = strong(HTML("Functional <br/> Enrichment")), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%")
+                        actionButton("b_qc", width = "100%", label = strong("QC"), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%", disable = TRUE),
+                        actionButton("b_exploratory_analysis", width = "100%",label = strong(HTML("Exploratory <br/> Analysis")), class = "btn-primary", style = "color:#fff; padding:20px; font-size:300%"),
+                        actionButton("b_dmp_dmr", width = "100%", label = strong("DMPs/DMRs"), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%"),
+                        actionButton("b_functional_enrichment", width = "90%",label = strong(HTML("Functional <br/> Enrichment")), class = "btn-primary", style = "color:#fff; padding:20px; font-size:300%")
                     ),
                     splitLayout(style = "margin-left: 14px; margin-right: 14px", cellArgs = list(style = "padding: 4px;"),
-                        actionButton("b_survival", width = "100%", label = strong("Survival"), class = "btn-primary", style = "color:#fff; padding:80px; font-size:300%"),
-                        actionButton("b_predicted_models", width = "100%", label = strong(HTML("Predicted <br/> Models")), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%"),
-                        actionButton("b_external_sources", width = "100%", label = strong(HTML("External <br/> Sources")), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%"),
-                        actionButton("b_genome_browser", width = "90%", label = strong(HTML("Genome <br/> Browser")), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%")
-                    )
+                        actionButton("b_survival", width = "100%", label = strong("Survival"), class = "btn-primary", style = "color:#fff; padding:50px; font-size:300%"),
+                        actionButton("b_predicted_models", width = "100%", label = strong(HTML("Predicted <br/> Models")), class = "btn-primary", style = "color:#fff; padding:20px; font-size:300%"),
+                        actionButton("b_external_sources", width = "100%", label = strong(HTML("External <br/> Sources")), class = "btn-primary", style = "color:#fff; padding:20px; font-size:300%"),
+                        actionButton("b_genome_browser", width = "90%", label = strong(HTML("Genome <br/> Browser")), class = "btn-primary", style = "color:#fff; padding:20px; font-size:300%")
+                    ))
                     
                 )
             ),
             tabItem(
                 tabName = "qc",
-                fluidPage(h1("QC"))
+                verticalLayout(
+                    boxPlus(title = "NORMALIZATION OPTIONS",
+                            collapsible = FALSE, closable = FALSE, width = 12,
+                            selectInput("select_minfi_norm", "Select Normalization", norm_options),
+                            div(
+                                margin_left = "50px",
+                                switchInput(
+                                    inputId = "select_minfi_dropcphs",
+                                    label = "Drop CpHs",
+                                    labelWidth = "fit",
+                                    value = TRUE,
+                                    inline = TRUE
+                                ),
+                                
+                                switchInput(
+                                    inputId = "select_minfi_dropsnps",
+                                    label = "Drop SNPs",
+                                    labelWidth = "fit",
+                                    value = TRUE,
+                                    inline = TRUE
+                                )
+                            ),
+                            
+                            conditionalPanel(
+                                "input.select_minfi_dropsnps",
+                                sliderInput(
+                                    inputId = "slider_minfi_maf",
+                                    label = "Minimum MAF to filter",
+                                    min = 0,
+                                    max = 1,
+                                    step = 0.01,
+                                    value = 0,
+                                    width = "75%"
+                                )
+                            ),
+                            
+                            switchInput(
+                                inputId = "select_minfi_chromosomes",
+                                label = "Drop X/Y Chr.",
+                                labelWidth = "fit",
+                                value = FALSE
+                            ),
+                            shinyjs::disabled(actionButton("button_minfi_select", "Select")),
+                            h4()
+                    ),
+                    # Box1
+                    sidebarPanel(
+                        width = 3,
+                        selectInput("select_minfi_norm", "Select Normalization", norm_options),
+                        div(
+                            margin_left = "50px",
+                            switchInput(
+                                inputId = "select_minfi_dropcphs",
+                                label = "Drop CpHs",
+                                labelWidth = "fit",
+                                value = TRUE,
+                                inline = TRUE
+                            ),
+                            
+                            switchInput(
+                                inputId = "select_minfi_dropsnps",
+                                label = "Drop SNPs",
+                                labelWidth = "fit",
+                                value = TRUE,
+                                inline = TRUE
+                            )
+                        ),
+                        
+                        conditionalPanel(
+                            "input.select_minfi_dropsnps",
+                            sliderInput(
+                                inputId = "slider_minfi_maf",
+                                label = "Minimum MAF to filter",
+                                min = 0,
+                                max = 1,
+                                step = 0.01,
+                                value = 0,
+                                width = "75%"
+                            )
+                        ),
+                        
+                        switchInput(
+                            inputId = "select_minfi_chromosomes",
+                            label = "Drop X/Y Chr.",
+                            labelWidth = "fit",
+                            value = FALSE
+                        ),
+                        
+                        shinyjs::disabled(actionButton("button_minfi_select", "Select")),
+                        h4(),
+                        textOutput("text_minfi_probes")
+                    ),
+                    
+                    
+                    mainPanel(
+                        width = 9,
+                        tabsetPanel(
+                            
+                            ###################################################################################
+                            
+                            tabPanel(
+                                "Control Type",
+                                selectInput("controlType", "Choose a control type:",
+                                            choices = c(
+                                                "BISULFITE CONVERSION I",
+                                                "BISULFITE CONVERSION II",
+                                                "HYBRIDIZATION",
+                                                "SPECIFICITY I",
+                                                "SPECIFICITY II",
+                                                "TARGET REMOVAL",
+                                                "BISULFITE CONVERSION I",
+                                                "EXTENSION",
+                                                "STAINING",
+                                                "NON-POLYMORPHIC"
+                                            ), selected = 1),
+                                selectInput("select_slide", "Select slide:", choices = c()),
+                                withSpinner(plotOutput("controlTypePlotGreen")),
+                                withSpinner(plotOutput("controlTypePlotRed"))
+                            ),
+                            
+                            ###################################################################################
+                            
+                            tabPanel(
+                                "Quality Control",
+                                h4("Overall Signal"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_qcraw")),
+                                h4("Bisulfite Conversion II"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_bisulfiterawII"))
+                            ),
+                            
+                            tabPanel(
+                                "Density plot",
+                                selectInput("probeType", "Choose a probe type for the density curves:",
+                                            choices = c("I-Green","I-Red","II"),
+                                            selected="I-Green"),
+                                textOutput("prueba1"),
+                                textOutput("prueba2"),
+                                textOutput("prueba3"),
+                                textOutput("prueba4"),
+                                h4("Raw"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_densityplotraw")),
+                                h4("Processed"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_densityplot"))
+                            ),
+                            tabPanel(
+                                "Failed probes",
+                                h4("Failure Rate Plot"),
+                                textOutput("text"),
+                                withSpinner(plotly::plotlyOutput("failure_rate_plot"))
+                            ),
+                            tabPanel(
+                                "Sex prediction",
+                                h4("X vs Y chromosomes signal plot"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_sex")),
+                                withSpinner(DT::DTOutput("table_minfi_sex"))
+                            ),
+                            tabPanel(
+                                "Correlations",
+                                h4("Processed"),
+                                withSpinner(plotly::plotlyOutput("graph_minfi_corrplot")),
+                                selectInput("select_minfi_typecorrplot", "Select data to plot", choices = c("p.value", "correlation value"), selected = "correlation value"),
+                                withSpinner(DT::DTOutput("table_minfi_corrplot"))
+                            )
+                        )
+                    )
+                )
             ),
             tabItem(
                 tabName = "exploratory_analysis",
